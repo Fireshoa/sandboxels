8c85b2493e5ff3c39aa84a5056284d3a72c74ebd
Fireshoa <29lindsayr@apcsd.org>
2025-04-08 12:00:54 -0400

    Update apple.js

--- mods/apple.js
+++ mods/apple.js
@@ -1,3 +1,18 @@
+elements.neutron_generator = {
+  color: "#505050",
+  behavior: [
+    "XX|XX|XX",
+    "XX|XX|XX",
+    "XX|XX|XX"
+  ],
+  behaviorOn: [
+    "XX|CR:neutron|XX",
+    "CR:neutron|XX|CR:neutron",
+    "XX|CR:neutron|XX"
+  ]
+  category: "weapons"
+}
+  
 elements.hypernuke = {
   color: "#FF0000",
   behavior: [
