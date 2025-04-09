525a74593d19fcaf5ed35bf25fc19371c6eff38f
Fireshoa <29lindsayr@apcsd.org>
2025-04-09 07:20:35 -0400

    Update breaktheworld.js

--- mods/breaktheworld.js
+++ mods/breaktheworld.js
@@ -8,6 +8,15 @@ elements.super_bomb = {
     "XX|XX|XX",
     "M2|M1 AND EX:50>explosion|M2"
   ],
-  category: "nukes"
+  category: "Nukes"
+}
+elements.nuker = {
+  color: "505050",
+  behavior: [
+    "XX|XX|XX",
+    "XX|XX|M1 AND FX",
+    "XX|CR:super_bomb|XX"
+  ],
+  category: "Nukes"
 }
 elements.wire.reactions.water = {elem1: "explosion", elem2: "gold_coin"}
