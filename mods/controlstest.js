4b134f6ce0351b64ba84126dcddacfd854029d0f
Fireshoa <29lindsayr@apcsd.org>
2025-04-09 21:40:52 -0400

    Create controlstest.js

---
+++ mods/controlstest.js
@@ -0,0 +1,19 @@
+keybinds["keyW"] = function() {
+  w = true;
+}
+renderer = {};
+renderer.tall=function(pixel,ctx drawSquare(ctx,pixel.color,pixel.x,pixel,y);
+drawSquare(ctx,pixel.color,pixel.x,pixel.y+1);}
+
+elements.jumptest = {
+  color: "#ffffff",
+  behavior: behaviors.STURDYPOWDER,
+  tick: function(pixel) {
+    if(w) {
+      pixel.y = pixel.y+elements[pixel.element].jump
+      w = false;
+    }
+  },
+  conduct: 30,
+  category: "Controllables"
+}
