8116c044bb4e6951bff075f9d8f70f7cd4a41336
Fireshoa <29lindsayr@apcsd.org>
2025-09-11 14:20:21 -0400

    Update survival_mech.js

--- mods/survival_mech.js
+++ mods/survival_mech.js
@@ -1,4 +1,4 @@
 elements.molten_copper.reactions.molten_aluminum = {elem1:"wire", elem2:null}
 elements.molten_zinc.reactions.wire = {elem1:"fuse", elem2:null}
 elements.poison.reactions.wire = {elem1:"battery", elem2:null}
-elements.molten_glass.reactions.wire = {elem1:"lightbulb", elem2:null}
+elements.molten_glass.reactions.wire = {elem1:"light_bulb", elem2:null}
