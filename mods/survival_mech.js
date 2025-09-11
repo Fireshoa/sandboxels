2ebed10a4772333cca9d83af4ccbc52919b00896
Fireshoa <29lindsayr@apcsd.org>
2025-09-11 10:36:48 -0400

    Create survival_mech.js

---
+++ mods/survival_mech.js
@@ -0,0 +1,4 @@
+elements.molten_copper.reactions.molten_gold = {elem1:"wire", elem2:null}
+elements.molten_copper.reactions.molten_brass = {elem1:"fuse", elem2:null}
+elements.molten_copper.reactions.molten_zinc = {elem1:"battery", elem2:null}
+elements.molten_copper.reactions.molten_glass = {elem1:"led_red", elem2:null}
